24f4ad823c37cd00b77fec1890e0efad31541cd1
aui <g-keardkeawfa@rozetta.jp>
2020-11-15 23:03:56 +0900

    finish google flip oversize pancake - untest

--- Google/CodeJam2017/QualificationRound/OversizedPancakeFlipper/Program.cs
+++ Google/CodeJam2017/QualificationRound/OversizedPancakeFlipper/Program.cs
@@ -16,7 +16,7 @@ public class Solution {
         int T = Int32.Parse(test_case);
         result.Add(test_case);
         for(int i=0;i<T;i++){
-            result.Add(Console.ReadLine);
+            result.Add(Console.ReadLine());
         }
         return result;
     }
@@ -26,16 +26,33 @@ public class Solution {
     }
         
     static string solution(string S, int K){
-        int result = 3;
+        int result = 0;
+        char[] pancakes = S.ToCharArray();
+        for(int index = 0;index <= (pancakes.Count() - K);index++){
+          //Console.WriteLine($"{index}:{pancakes[index]} , {pancakes.Count() - K}");
+          if (pancakes[index] == '-') {
+            for(int flip=0;flip<K;flip++){
+              if (pancakes[index+flip] == '-'){
+                pancakes[index+flip] = '+';
+              } else {
+                pancakes[index+flip] = '-';
+              }
+            }
+            result ++;
+          }
+        }
         
-        
+        //Console.WriteLine($"{S} to {string.Join(",",pancakes.ToArray())}");
+
         return result.ToString();   
     }
 
     static void DoSolving(){
       List<string> arr = (!DEBUG)? ReadInput() : MockData();
 
-      for(int i=0;i<arr.Count();i++){
+      int cases = Int32.Parse(arr[0]);
+
+      for(int i=1;i<cases+1;i++){
         var item = arr[i].Split(' ');
         string S = item[0];
         int K = Int32.Parse(item[1]);
