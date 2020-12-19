bd0f14bd9981f2467e0b5ffabbad85fbf4bf92c5
aui <g-keardkeawfa@rozetta.jp>
2020-12-19 15:33:08 +0900

    add completed hackerrank

---
+++ Hackerrank/GradingProblem/solution.cs
@@ -0,0 +1,73 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'gradingStudents' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY grades as parameter.
+     */
+
+    public static List<int> gradingStudents(List<int> grades)
+    {
+        //Console.WriteLine(string.Join(",", grades));
+        List<int> result = new List<int>();
+        foreach(int point in grades){
+            if (point < 38){
+                result.Add(point);
+                continue;
+            }
+            
+            int round_up = point % 5;
+            //Console.WriteLine(round_up);
+            if(round_up > 2){
+                round_up = 5 - round_up;
+            } else {
+                round_up = 0;
+            }
+            
+            result.Add(point+round_up);
+        }
+        return result;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int gradesCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> grades = new List<int>();
+
+        for (int i = 0; i < gradesCount; i++)
+        {
+            int gradesItem = Convert.ToInt32(Console.ReadLine().Trim());
+            grades.Add(gradesItem);
+        }
+
+        List<int> result = Result.gradingStudents(grades);
+
+        textWriter.WriteLine(String.Join("\n", result));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
