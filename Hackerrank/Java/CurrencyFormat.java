f934c129671171416a56908c12523794c650e291
aui <g-keardkeawfa@rozetta.jp>
2021-01-12 21:21:39 +0900

    update java task

---
+++ Hackerrank/Java/CurrencyFormat.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.text.*;
+
+public class Solution {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+        
+        // Write your code here.
+        //Set<Currency> currencySet = Currency.getAvailableCurrencies();
+        //for(Currency c:currencySet){  
+        //    System.out.println(c);  
+        //}
+        
+        NumberFormat us_format = NumberFormat.getCurrencyInstance(Locale.US);  
+        String us = us_format.format(payment);
+        
+        NumberFormat in_format = NumberFormat.getCurrencyInstance(new Locale("en", "IN")); 
+        String india = in_format.format(payment);
+        
+        NumberFormat cn_format = NumberFormat.getCurrencyInstance(Locale.CHINA);
+        String china = cn_format.format(payment);
+        
+        NumberFormat eur_format = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+        String france = eur_format.format(payment);
+        
+        System.out.println("US: " + us);
+        System.out.println("India: " + india);
+        System.out.println("China: " + china);
+        System.out.println("France: " + france);
+    }
+}
